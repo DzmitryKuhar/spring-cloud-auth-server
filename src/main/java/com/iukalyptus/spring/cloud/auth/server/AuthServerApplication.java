c1fa26ec1b834ea5b4e19566f7a4ce575fb2c125
Dzmitry Kuhar <dzmitrykuhar@gmail.com>
2017-11-08 20:43:53 +0300

    Initial commit

---
+++ src/main/java/com/iukalyptus/spring/cloud/auth/server/AuthServerApplication.java
@@ -0,0 +1,19 @@
+package com.iukalyptus.spring.cloud.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * Date: 10/08/2017
+ *
+ * @author Dzmitry Kuhar
+ */
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+}
